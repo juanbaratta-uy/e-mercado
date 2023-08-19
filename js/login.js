b087b17264b95bb6ccdcf48fb08141d4a7a91549
Juan Baratta <juanpablobaratta@gmail.com>
2023-08-19 16:43:26 -0300

    Terminamos la primera entrega

--- js/login.js
+++ js/login.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", comprobar);
     function comprobar (){
         let usuario = document.getElementById("username").value;
         let clave = document.getElementById("password").value;
+
         if ((usuario !== "") && (clave !== "")) {
             window.location.href = "index.html";
+            localStorage.setItem('user',usuario);
         }
-    };
+    }
