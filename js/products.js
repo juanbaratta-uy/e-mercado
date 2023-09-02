866b7087b53820eb7ff5189ed7df270bd8b56db9
Juan Baratta <juanpablobaratta@gmail.com>
2023-09-02 19:27:24 -0300

    Terminamos la segunda entrega

--- js/products.js
+++ js/products.js
@@ -118,32 +118,4 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// get search bar element
-// const searchInput = document.getElementById("searchInput");
-
-// // store name elements in array-like object
-// const namesFromDOM = document.getElementsByClassName("name");
-
-// // listen for user events
-// searchInput.addEventListener("keyup", (event) => {
-//     const { value } = event.target;
-
-//     // get user search input converted to lowercase
-//     const searchQuery = value.toLowerCase();
-
-//     for (const nameElement of namesFromDOM) {
-//         // store name text and convert to lowercase
-//         let name = nameElement.textContent.toLowerCase();
-
-//         // compare current name to search input
-//         if (name.includes(searchQuery)) {
-//             // found name matching search, display it
-//             nameElement.style.display = "block";
-//         } else {
-//             // no match, don't display name
-//             nameElement.style.display = "none";
-//         }
-//     }
-// });
-
 
